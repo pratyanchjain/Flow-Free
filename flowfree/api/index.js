e8ef370ceb0e6e70e346e8b71c574b485bfc34f8
Pratyanch Jain <jain617@purdue.edu>
2024-08-10 00:40:42 +0530

    setting to correct routes

--- flowfree/api/index.js
+++ flowfree/api/index.js
@@ -181,7 +181,7 @@ function getRandomColor() {
 
 async function getBoard(boardSize) {
     try {
-        let response = await axios.post("http://localhost:3003/puzzle/", {size: boardSize});
+        let response = await axios.post("https://flow-free.onrender.com/puzzle/", {size: boardSize});
         return response.data;
     } catch (error) {
         return error;
--- flowfree/socket.js
+++ flowfree/socket.js
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3004");
+export const socket = io("https://flowfreeserver.onrender.com");
